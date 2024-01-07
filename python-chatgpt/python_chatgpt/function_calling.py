cb5a6b80e092b8dc84f94f87204ddb1e0584855f
Seungjoon Lee <joonlee0228@gmail.com>
2024-01-06 23:06:59 -0800

    added assistants api function calling

--- python-chatgpt/python_chatgpt/function_calling.py
+++ python-chatgpt/python_chatgpt/function_calling.py
@@ -20,32 +20,32 @@ def execute_function_call(message):
     return results
 
 tools = [
-  {
-    "type": "function",
-    "function": {
-      "name": "get_current_weather",
-      "description": "Get the current weather in a given location",
-      "parameters": {
-        "type": "object",
-        "properties": {
-          "location": {
-            "type": "string",
-            "description": "The city and state, e.g. San Francisco, CA",
-          },
-          "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
-        },
-        "required": ["location"],
-      },
+    {
+        "type": "function",
+        "function": {
+            "name": "get_current_weather",
+            "description": "Get the current weather in a given location",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The city and state, e.g. San Francisco, CA",
+                    },
+                    "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+                },
+                "required": ["location"],
+            },
+        }
     }
-  }
 ]
 messages = [{"role": "user", "content": "What's the weather like in Boston today?"}]
 
 completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=messages,
-  tools=tools,
-  tool_choice="auto"
+    model="gpt-3.5-turbo",
+    messages=messages,
+    tools=tools,
+    tool_choice="auto"
 )
 
 # https://platform.openai.com/docs/api-reference/chat
